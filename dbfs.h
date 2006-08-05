4569638405d7cc1bdd487a4c9e49ffa9dc7adfad
Jeff Garzik <jeff@garzik.org>
2006-08-05 16:12:34 -0400

    Add copyright header to source files.

--- dbfs.h
+++ dbfs.h
@@ -1,6 +1,24 @@
 #ifndef __DBFS_H__
 #define __DBFS_H__
 
+/*
+ * Copyright 2006 Jeff Garzik <jgarzik@pobox.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
 #include <glib.h>
 
 #define ALIGN(x,a) (((x)+(a)-1)&~((a)-1))
